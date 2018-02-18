2ad75fa145033d85a94e13b6db6b83b36dd96031
Zero <1069847044@qq.com>
2018-02-18 18:46:15 +0800

    prettify some 'return' condition

--- app/config.py
+++ app/config.py
@@ -49,9 +49,11 @@ class Config(object):
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S',
-                            filename=os.path.join(self.cache_dir, 'weekery.log'),
+                            filename=os.path.join(
+                                self.cache_dir, 'weekery.log'),
                             filemode='a')
-        logging.info('\n\n' + '=' * 5 + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '=' * 5)
+        logging.info('\n\n' + '=' * 5 +
+                     datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '=' * 5)
         if mkdir1:
             logging.info('C:/ProgramData not exist, created successfully')
         if mkdir2:
@@ -71,7 +73,7 @@ class Config(object):
             wiz_dir = self.wiz_dir
         # v[Exception 1] user change WizNote default data folder
         else:
-            showinfo('初始化：第1步(共5步)', 
+            showinfo('初始化：第1步(共5步)',
                      '选择您为知笔记的本地数据存储路径\n详见为知笔记->设置v->选项->数据存储')
             wiz_dir = ''
             loop = True
@@ -83,24 +85,27 @@ class Config(object):
                     if not ans:
                         logging.info('Gave up folder selection')
                         self.root.destroy()
-                        return
+                        loop = False
                 else:
-                    if os.path.exists(wiz_dir+'/Wiz.log') and os.path.exists(wiz_dir+'/Data'):
+                    if os.path.exists(wiz_dir + '/Wiz.log') and os.path.exists(wiz_dir + '/Data'):
                         self.wiz_dir = wiz_dir
                         loop = False
-                    elif not os.path.exists(wiz_dir+'/Data'):
+                    elif not os.path.exists(wiz_dir + '/Data'):
                         showwarning('警告', '[Data]文件夹不存在')
-                        logging.warning('['+wiz_dir+'/Data]'+'is not a wiz data folder')
+                        logging.warning(
+                            '[' + wiz_dir + '/Data]' + 'is not a wiz data folder')
                         self.root.destroy()
-                        return
+                        loop = False
                     else:
                         ans = askyesno('警告', '此文件夹非为知笔记数据文件夹, 重新选择？')
                         if not ans:
                             logging.info('Gave up reselect wiz data folder')
                             self.root.destroy()
-                            return
-        # ^[Exception 1] ends 
-        
+                            loop = False
+            else:
+                return
+        # ^[Exception 1] ends
+
     # >>> [Default] user select a correct WizNote folder, find User_email
         wiz_dir_data = wiz_dir + '/Data'
         dir_list = os.listdir(wiz_dir_data)
@@ -108,7 +113,7 @@ class Config(object):
         for items in dir_list:
             if not os.path.isfile(os.path.join(wiz_dir, items)):
                 emails_list.append(items)
-                
+
     # >>> [Default] user just use one account (only one email folder in "Data" )
         if len(emails_list) == 1:
             self.user_email = emails_list[0]
@@ -120,7 +125,7 @@ class Config(object):
             loop = True
             # ensure them input a right number
             while loop:
-                ans = askinteger('初始化：第2步(共5步)', 
+                ans = askinteger('初始化：第2步(共5步)',
                                  '选择当前用户名序号：\n' + str(diag_str))
                 if ans in diag_str.keys():
                     self.user_email = diag_str[ans]
@@ -131,7 +136,9 @@ class Config(object):
                     else:
                         showwarning('警告', '用户取消选择，初始化停止')
                         self.root.destroy()
-                        return
+                        loop = False
+            else:
+                return
         # ^[Exception 3] ends
         # v[Exception 4] there are no email folder in "Data" folder
         else:
@@ -139,7 +146,7 @@ class Config(object):
             self.root.destroy()
             return
         # ^[Exception 4] ends
-        
+
     # >>> [Default] select weekry folder
         wiz_dir_data_email = os.path.join(wiz_dir_data, self.user_email)
         showinfo('初始化：第3步(共5步)', '选择您的[周记]文件夹')
@@ -158,22 +165,22 @@ class Config(object):
                         pass
                 except ValueError:
                     pass
-                    
+
                 norm_weekery = os.path.normpath(weekery)
                 norm_wdde = os.path.normpath(wiz_dir_data_email)
-                
-                _, weekery_dir = norm_weekery.split(norm_wdde) 
-                
+
+                _, weekery_dir = norm_weekery.split(norm_wdde)
+
                 self.weekery_dir = weekery_dir
                 self.work_dir = norm_weekery
-                
+
                 loop = False
             else:  # cancel selection
                 ans = askyesno('警告', '您没有选择文件夹,重新选择？')
                 if not ans:
                     self.root.destroy()
                     return
-                
+
         self._write_config()
 
     def _read_config(self):
@@ -202,23 +209,25 @@ class Config(object):
         if extend:
             with open(self.config_path, 'w') as f:
                 config.write(f)
-        
+
         work_dir = self.wiz_dir + '\\Data\\' + self.user_email + self.weekery_dir
         if os.path.exists(work_dir):
             self.work_dir = work_dir
         else:
-            logging.info('['+work_dir+'] not exist when _read_config')
+            logging.info('[' + work_dir + '] not exist when _read_config')
             # find whether computer change lead to username change only
-            ans = askyesno('警告', '周记路径[' + work_dir + ']不存在，重新初始化(Y)或手动编辑配置文件(N)？')
+            ans = askyesno(
+                '警告', '周记路径[' + work_dir + ']不存在，重新初始化(Y)或手动编辑配置文件(N)？')
             if not ans:  # reedit
                 showinfo('提示', '周记配置文件config.ini路径：\n' + self.config_path)
                 logging.info('user selected to modify config mannually')
                 self.root.destroy()
                 return
             else:
-                logging.info('user selected to initialize config automatically')
+                logging.info(
+                    'user selected to initialize config automatically')
                 self._initialize_config()
-    
+
     def _write_config(self):
         config = ConfigParser()
         config.read(self.config_path)
@@ -229,13 +238,13 @@ class Config(object):
         config.set('main', 'language', self.language)
         config.set('main', 'last_read', str(self.last_read))
         config.set('main', 'color_kind', str(self.color_kind))
-        
+
         with open(self.config_path, 'w') as f:
             config.write(f)
 
         logging.info('Custom config file "config.ini" has been created')
         showinfo('初始化：第4步(共5步)', '配置文件初始化完成, 即将加载周记文件！')
-        
+
     def update_config(self):
         config = ConfigParser()
         config.read(self.config_path)
@@ -245,7 +254,7 @@ class Config(object):
         config.set('main', 'language', self.language)
         config.set('main', 'last_read', str(self.last_read))
         config.set('main', 'color_kind', str(self.color_kind))
-        
+
         with open(self.config_path, 'w+') as f:
             config.write(f)
 
@@ -255,5 +264,5 @@ if __name__ == '__main__':
     root = Tk()
     root.title('ImageDBH')
     root.config(bg='white')
-    conf = Config()
+    conf = Config(root)
     root.mainloop()
